3c93688e008651ae184741dc74336cbeeb7c1627
Yuhau Lin <>
2025-10-14 04:40:27 -0400

    github-pages

--- vite.config.js
+++ vite.config.js
@@ -3,4 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  base: '/fire/', // Change this to your repo name, or '/' if deploying to username.github.io
 })
